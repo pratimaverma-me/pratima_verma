f8f58a11785016b456c24e226a3adfff9b4b0ccf
Pratima <pratimaverma711@gmail.com>
2026-07-23 12:56:14 +0530

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,13 +7,24 @@ import { Contact } from "@/components/sections/Contact";
 
 export default function Home() {
   return (
-    <>
-      <About />
-      <Experience />
-      <Projects />
-      <Skills />
-      <Education />
-      <Contact />
-    </>
+    <div className="min-h-screen lg:grid lg:grid-cols-[minmax(300px,38%)_minmax(0,1fr)]">
+      {/* Left sidebar */}
+      <aside className="w-full lg:sticky lg:top-0 lg:h-screen">
+        <div className="h-full px-6 py-10 lg:flex lg:flex-col lg:justify-between lg:px-10 lg:py-16">
+          <About />
+        </div>
+      </aside>
+
+      {/* Right content */}
+      <main className="min-w-0 w-full px-6 pb-20 lg:px-10 lg:py-16">
+        <div className="mx-auto flex w-full max-w-4xl flex-col gap-24">
+          <Experience />
+          <Projects />
+          <Skills />
+          <Education />
+          <Contact />
+        </div>
+      </main>
+    </div>
   );
 }
